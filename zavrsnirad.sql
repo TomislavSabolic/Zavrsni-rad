f40323ba5c29bf484839efa1b08b4eba1d2a0cee
TomislavSabolic <37838708+TomislavSabolic@users.noreply.github.com>
2021-11-20 21:16:06 +0100

    kreiranje tablice klijent

--- zavrsnirad.sql
+++ zavrsnirad.sql
@@ -1,2 +1,13 @@
 drop database if exists zavrsnirad;
 create database zavrsnirad character set utf8;
+
+use zavrsnirad;
+
+create table klijent(
+    sifra int not null primary key auto_increment,
+    ime varchar(50)not null,
+    prezime varchar(50) not null,
+    oib char(11),
+    email varchar(50)
+
+);
